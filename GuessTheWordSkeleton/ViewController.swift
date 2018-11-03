c6c25a78c17133b2ad0069fa11e2e4b3139b046d
Will Oakley <woakley5@icloud.com>
2018-11-02 17:38:00 -0700

    Init

---
+++ GuessTheWordSkeleton/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GuessTheWordSkeleton
+//
+//  Created by Will Oakley on 11/2/18.
+//  Copyright © 2018 Will Oakley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
